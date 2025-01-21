dc8cfdb36f67562b5c0f1855970992b36f424f6d
Priyam Shah <108848788+priyam-02@users.noreply.github.com>
2025-01-21 11:04:59 -0500

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://priyam123:priyam123@cluster0.xjddk.mongodb.net/food_delivery').then(()=>console.log('DB Connected'));
+    await mongoose.connect('//Enter your database connection link here').then(()=>console.log('DB Connected'));
 }
